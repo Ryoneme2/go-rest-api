f06001d6d4f566a8d2912896b310c30084f94fa2
Ryoneme2 <worknarair6@gmail.com>
2022-04-19 23:30:55 +0700

    auto reload

--- src/github.com/Ryoneme2/go-restful-api/cmd/server/main.go
+++ src/github.com/Ryoneme2/go-restful-api/cmd/server/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"../../pkg/greeting"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	greet := greeting.Greeting()
-
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": greet,
+			"message": "Hello2",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
